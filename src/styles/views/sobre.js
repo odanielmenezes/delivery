1e95d62516534b549504456cdd6cf2802ff68a32
Daniel de Menezes <daniel.menezes@infoar.com.br>
2022-03-13 17:44:29 -0300

    add pagina sobre

---
+++ src/styles/views/sobre.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+import Main_Walpapper from '../../assets/fundo_main.jpg'
+
+
+const sobre_page = styled.div`
+.container {
+    width: 100vw;
+    height: 100vh;
+    background-image: url(${Main_Walpapper});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-color:#fff;
+    min-height: 100vh;
+
+    .response {
+        height: 85vh;
+        width: 100vw;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        
+        img.title-sobre {
+                width: 800px;
+        }
+
+        .conteudo-sobre {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        .text-sobre {
+            overflow-y: scroll;
+            background-color: rgba(0,0,0, 0.5);
+            padding: 40px 50px;
+            max-height: 45vh;
+            border: 2px solid #FF4500;
+            border-radius: 10px;
+            width: 45%;
+            text-align: justify;
+            color: #FFF;
+        }
+        .text-sobre::-webkit-scrollbar {
+            width: 10px;               /* width of the entire scrollbar */
+        }
+
+        .text-sobre::-webkit-scrollbar-track {
+            background: transparent;        /* color of the tracking area */
+        }
+
+        .text-sobre::-webkit-scrollbar-thumb {
+            background-color: #000;    /* color of the scroll thumb */
+            border-radius: 20px;       /* roundness of the scroll thumb */
+            border: 1px solid #FF4500;  /* creates padding around scroll thumb */
+        }
+    }
+}
+@media (max-width: 450px) {
+    .text-sobre {
+        width: 70vw !important;
+        max-height: 55vh !important;
+    }
+    img.title-sobre {
+        width: 400px !important;
+    }
+}
+`
+export default sobre_page;
